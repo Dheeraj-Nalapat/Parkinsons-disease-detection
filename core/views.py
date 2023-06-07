296e1bb6d848027fb4bc13b71933d023066ba001
Aparna <aparnaanand171@gmail.com>
2023-06-07 16:10:47 +0530

    commit

--- core/views.py
+++ core/views.py
@@ -9,7 +9,26 @@ def index(request):
     return render(request,'index.html')
 
 def login(request):
+
+    if request.method=='POST':
+        username=request.POST['email']
+        password=request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'credentials invalid')
+            return redirect('login')
     return render(request,'loginpage.html')
+
+def settings(request):
+    return render(request,'settings.html')
+
+def userpage(request):
+    return render(request,'userpage.html')
 
 
 def signup(request):
@@ -25,14 +44,14 @@ def signup(request):
             messages.info(request,'Email taken')
             return redirect('signup')
         else:
-            user =  User.objects.create_user(username=firstname, email=email,password=password)
+            user =  User.objects.create_user(username=email, email=email,password=password)
             user.save()
 
 
-            user_model = User.objects.get(username=firstname)
-            new_profile = Profile.objects.create(user=user_model, idusers=user_model.id)
+            user_model = User.objects.get(username=email)
+            new_profile = Profile.objects.create(user=user_model, idusers=user_model.id,firstname=firstname,lastname=lastname,age=age,gender=gender,)
             new_profile.save()
-            return redirect('signup')
+            return redirect('login')
         
     else:
         return render(request,'signup.html')
