9f055f5e0d8670e3e07a63fddc8ff8c58b24c6f4
Aparna <aparnaanand171@gmail.com>
2023-06-07 19:06:00 +0530

    backend url linked

--- core/views.py
+++ core/views.py
@@ -1,35 +1,14 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
-
-def login(request):
-
-    if request.method=='POST':
-        username=request.POST['email']
-        password=request.POST['password']
-
-        user=auth.authenticate(username=username,password=password)
-
-        if user is not None:
-            auth.login(request,user)
-            return redirect('/')
-        else:
-            messages.info(request,'credentials invalid')
-            return redirect('login')
-    return render(request,'loginpage.html')
-
-def settings(request):
-    return render(request,'settings.html')
-
-def userpage(request):
-    return render(request,'userpage.html')
-
 
 def signup(request):
     if request.method == 'POST':
@@ -48,10 +27,46 @@ def signup(request):
             user.save()
 
 
+             #log user in and redirect to settings page
+            user_login = auth.authenticate(username=email, password=password)
+            auth.login(request, user_login)
+
+
             user_model = User.objects.get(username=email)
             new_profile = Profile.objects.create(user=user_model, idusers=user_model.id,firstname=firstname,lastname=lastname,age=age,gender=gender,)
             new_profile.save()
-            return redirect('login')
+            return redirect('settings')
         
     else:
         return render(request,'signup.html')
+
+def signin(request):
+
+    if request.method=='POST':
+        username=request.POST['email']
+        password=request.POST['password']
+
+        user=auth.authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('/userpage')
+        else:
+            messages.info(request,'credentials invalid')
+            return redirect('login')
+    return render(request,'loginpage.html')
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request,'settings.html')
+
+@login_required(login_url='signin')
+def userpage(request):
+    return render(request,'userpage.html')
+
+
+
+@login_required(login_url='signin')    
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
